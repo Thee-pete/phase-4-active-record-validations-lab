71de28c0688791a626be0426a37877c78534c312
Peter Ndumia <ndegwandumia@gmail.com>
2022-09-29 18:35:42 +0300

    Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :clickbait
+
+    def clickbait
+        if title == "True Facts"
+            errors.add(:title, "Add a clickbait-y title")
+        end
+    end
+
+
 end
